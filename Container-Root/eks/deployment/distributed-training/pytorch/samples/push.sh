f2a90147a9d8ff3b3fd3d38fa74a5c2e25997b5b
Ubuntu <ubuntu@ip-172-31-21-125.us-west-2.compute.internal>
2022-06-18 01:16:18 +0000

    adding efa docker and also refactoring code

---
+++ Container-Root/eks/deployment/distributed-training/pytorch/samples/push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+######################################################################
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. #
+# SPDX-License-Identifier: MIT-0                                     #
+######################################################################
+
+if [ -f /aws-do-eks/.env ]; then
+    pushd /aws-do-eks
+else
+    pushd ../../../../
+fi
+source .env
+popd
+
+if [ -z $REGISTRY ]; then
+    echo 'REGISTRY not defined in .env file. Exiting.'
+    exit
+else
+    echo 'Using' $REGISTRY
+fi
+
+# Push Docker image
+docker push ${REGISTRY}pytorch-efa
