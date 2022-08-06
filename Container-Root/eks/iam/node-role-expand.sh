a725d19256b65cf096c1cb5439b20bfdceb5b998
Alex Iankoulski <iankouls@amazon.com>
2022-08-05 18:50:59 -0700

    Fix node expansion and update cfn template

--- Container-Root/eks/iam/node-role-expand.sh
+++ Container-Root/eks/iam/node-role-expand.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "$CLUSTER_NAME" = "" ]; then
-	echo "Please export CLUSTER_NAME before running this script"
+if [ "${CLUSTER_NAME}" = "" ]; then
+        echo "Please export CLUSTER_NAME before running this script"
 else
-	export NODE_IAM_ROLE_NAME=$(eksctl get iamidentitymapping --cluster ${AWS_CLUSTER_NAME} | grep  arn | awk  '{print $1}' | egrep -o eks.*)
-	aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonSageMakerFullAccess
-	aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
-	aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
-	aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess
+        echo "Attaching policies to node role for cluster ${CLUSTER_NAME} ..."
+        export NODE_IAM_ROLE_NAME=$(eksctl get iamidentitymapping --cluster ${CLUSTER_NAME} | grep  arn | awk  '{print $1}' | egrep -o eks.*)
+        aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonSageMakerFullAccess
+        aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+        aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
+        aws iam attach-role-policy --role-name ${NODE_IAM_ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess
 fi
 
