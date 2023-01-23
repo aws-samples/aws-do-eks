96a7ce523b0f39a70f299e7df4f6c85ab15fe69d
iankouls-aws <iankouls@amazon.com>
2023-01-23 18:36:07 +0000

    Update eksctl version to 0.126.0

--- Container-Root/eks/ops/setup/install-eksctl.sh
+++ Container-Root/eks/ops/setup/install-eksctl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl --location "https://github.com/weaveworks/eksctl/releases/download/v0.106.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+#curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+curl --location "https://github.com/weaveworks/eksctl/releases/download/v0.126.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 eksctl version
 
