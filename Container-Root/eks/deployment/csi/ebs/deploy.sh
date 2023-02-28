68b0e26e667e45be046ef325ef3d81839bdae5a7
iankouls-aws <iankouls@amazon.com>
2023-02-28 09:21:50 +0000

    Add ebs csi controller, fix gpu-operator, improve stable-diffusion sample

---
+++ Container-Root/eks/deployment/csi/ebs/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Reference: https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/docs/install.md
+
+kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.16"
+
--- Container-Root/eks/deployment/nvidia-gpu-operator/deploy.sh
+++ Container-Root/eks/deployment/nvidia-gpu-operator/deploy.sh
@@ -6,5 +6,5 @@
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia && helm repo update
 
 # Install in gpu-operator namespace
-helm install --wait --generate-name -n gpu-operator --create-namespace --set driver.enabled=false --set toolkit-enabled=false --set migManager.enabled=false --set psp.enabled=false --set mig.strategy=single --set nfd.enabled=true nvidia/gpu-operator
+helm install --wait --generate-name -n gpu-operator --create-namespace --set driver.enabled=false --set toolkit.enabled=false --set migManager.enabled=false --set psp.enabled=false --set mig.strategy=single --set nfd.enabled=true nvidia/gpu-operator
 
