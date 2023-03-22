04bb2705542b4badaa4f5811299f9431889e375f
Alex Iankoulski <iankouls@amazon.com>
2023-03-22 04:29:54 +0000

    Enable checkpointing, restart etcd pod

--- Container-Root/eks/deployment/distributed-training/pytorch/pytorchjob/imagenet/stop-cpu.sh
+++ Container-Root/eks/deployment/distributed-training/pytorch/pytorchjob/imagenet/stop-cpu.sh
@@ -2,3 +2,5 @@
 
 kubectl delete -f ./imagenet-cpu.yaml
 
+kubectl delete pod $(kubectl get pod | grep etcd | cut -n ' ' -f 1)
+
--- Container-Root/eks/deployment/distributed-training/pytorch/pytorchjob/imagenet/stop-gpu.sh
+++ Container-Root/eks/deployment/distributed-training/pytorch/pytorchjob/imagenet/stop-gpu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kubectl delete -f ./imagenet-gpu.sh
+kubectl delete -f ./imagenet-gpu.yaml
+
+kubectl delete pod $(kubectl get pod | grep etd | cut -d ' ' -f 1)
 
