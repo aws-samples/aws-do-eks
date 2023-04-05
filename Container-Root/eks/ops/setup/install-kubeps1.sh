faf01732ef793934f2db257ff2fd53f1f57b998b
Alex Iankoulski <iankouls@amazon.com>
2023-04-05 00:22:30 +0000

    Add prepull examples and ops aliases

--- Container-Root/eks/ops/setup/install-kubeps1.sh
+++ Container-Root/eks/ops/setup/install-kubeps1.sh
@@ -14,6 +14,14 @@ alias kt='kubetail'
 alias ks='kubectl node-shell'
 alias nv='eks-node-viewer'
 alias tx='torchx'
+alias wp='watch-pods.sh'
+alias wn='watch-nodes.sh'
+alias wnt='watch-node-types.sh'
+alias lp='pods-list.sh'
+alias ln='nodes-list.sh'
+alias lnt='nodes-types-list.sh'
+alias pe='pod-exec.sh'
+alias pl='pod-logs.sh'
 
 if [ -f ~/.kubeon ]; then
         source ~/kube-ps1.sh
