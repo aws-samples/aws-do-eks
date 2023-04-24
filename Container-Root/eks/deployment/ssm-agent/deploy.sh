a5eac23639e32cb9433e57f389ac9ed7b6f3f375
iankouls-aws <iankouls@amazon.com>
2023-04-24 23:25:56 +0000

    Add reference link to ssm agent install

--- Container-Root/eks/deployment/ssm-agent/deploy.sh
+++ Container-Root/eks/deployment/ssm-agent/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Reference: https://docs.aws.amazon.com/prescriptive-guidance/latest/patterns/install-ssm-agent-on-amazon-eks-worker-nodes-by-using-kubernetes-daemonset.html
+
 echo ""
 echo "Deploying SSM Agent daemonset ..."
 
