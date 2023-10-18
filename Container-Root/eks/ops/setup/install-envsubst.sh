d875fdd1fee466d6a763efc48862e40c3254ecee
iankouls-aws <iankouls@amazon.com>
2023-10-18 20:57:56 +0000

    Fix envsubst install on Graviton

--- Container-Root/eks/ops/setup/install-envsubst.sh
+++ Container-Root/eks/ops/setup/install-envsubst.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o envsubst
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+	GOARCH=arm64
+else
+	GOARCH=amd64
+fi
+PLATFORM=$(uname -s)-$GOARCH
+
+curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-${PLATFORM} -o envsubst
 chmod +x envsubst
 sudo mv envsubst /usr/local/bin
 
