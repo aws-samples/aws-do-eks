74a8c6da052dd9c9945fb26ef318c0393cadb561
iankouls-aws <iankouls@amazon.com>
2023-10-19 08:22:32 +0000

    Docker install using yum

--- Container-Root/eks/ops/setup/install-docker-yum.sh
+++ Container-Root/eks/ops/setup/install-docker-yum.sh
@@ -6,14 +6,21 @@
 ######################################################################
 
 if [ -f /usr/bin/yum ]; then
+    echo ""
+    echo "Installing Docker ..."
     yum update
-    yum install docker -y
+    sudo yum install docker -y
     usermod -a -G docker ec2-user
     id ec2-user
     newgrp docker
+    systemctl enable docker.service
     systemctl enable docker
-    sleep 2
+    sleep 5
+    systemctl start docker.service
     systemctl start docker
+    echo ""
+    echo "Done installing Docker."
+    echo ""
 else
     echo "/usr/bin/yum does not exist"
     echo "Cannot install Docker cli with this script"
