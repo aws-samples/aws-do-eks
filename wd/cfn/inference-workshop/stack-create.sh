f48e6812114e72e1b4e2530b5229103ea9b9e301
iankouls-aws <iankouls@amazon.com>
2023-10-19 03:21:11 +0000

    Ubuntu base arm management instance

--- wd/cfn/inference-workshop/stack-create.sh
+++ wd/cfn/inference-workshop/stack-create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name ManagementInstance --template-body file://ManagementInstance.json --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name ManagementInstance --template-body file://ManagementInstance.json --capabilities CAPABILITY_IAM --parameters ParameterKey="REGION",ParameterValue="us-west-2"
 
