8b4015a5e6e5c7a38c250ec4977226bb5935e685
iankouls-aws <iankouls@amazon.com>
2023-10-19 09:10:19 +0000

    Create Amazon Linux 2023 management instance instead of Ubuntu

--- wd/cfn/inference-workshop/stack-create.sh
+++ wd/cfn/inference-workshop/stack-create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name ManagementInstance --template-body file://ManagementInstance.json --capabilities CAPABILITY_IAM --parameters ParameterKey="REGION",ParameterValue="us-west-2"
+aws cloudformation create-stack --stack-name ManagementInstance --template-body file://ManagementInstance.json --capabilities CAPABILITY_IAM 
 
