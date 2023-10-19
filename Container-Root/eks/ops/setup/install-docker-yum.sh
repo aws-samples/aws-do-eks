e1eb420d78c9874e154b7fd5e0f943c508a3b355
iankouls-aws <iankouls@amazon.com>
2023-10-19 03:46:23 +0000

    Add docker install for AL2 on Graviton

---
+++ Container-Root/eks/ops/setup/install-docker-yum.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+######################################################################
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. #
+# SPDX-License-Identifier: MIT-0                                     #
+######################################################################
+
+if [ -f /usr/bin/yum ]; then
+    yum update
+    yum install docker -y
+    usermod -a -G docker ec2-user
+    id ec2-user
+    newgrp docker
+    systemctl enable docker.service
+    sleep 2
+    systemctl start docker.service
+else
+    echo "/usr/bin/yum does not exist"
+    echo "Cannot install Docker cli with this script"
+    echo "Please refer to https://docs.docker.com/engine/install"
+fi
+
