0aa746b9518a517baa2319e6a5e4e94b72d2045a
iankouls-aws <iankouls@amazon.com>
2024-01-25 01:43:30 +0000

    Update cloud9 install

--- Container-Root/eks/ops/setup/install-cloud9.sh
+++ Container-Root/eks/ops/setup/install-cloud9.sh
@@ -15,8 +15,9 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | b
 source ~/.nvm/nvm.sh
 source ~/.bashrc
 nvm install 16.15.1
+username=$(whoami)
 if [ ! -f /usr/bin/node ]; then
- sudo ln -s /home/ubuntu/.nvm/versions/node/v16.15.1/bin/node /usr/bin/node
+        sudo ln -s /home/${username}/.nvm/versions/node/v16.15.1/bin/node /usr/bin/node
 fi
 node --version
 
@@ -24,18 +25,26 @@ node --version
 curl -o /tmp/install.sh -L https://raw.githubusercontent.com/c9/install/master/install.sh
 chmod +x /tmp/install.sh
 pushd /tmp
-./install.sh -d /opt/c9
+mkdir -p ~/.c9
+./install.sh -d ~/.c9
 popd
 
 # Install tmux script
-sudo ln -s -f /opt/c9/bin/tmux /usr/bin/tmux
+C9_HOME=~/.c9
+if [ ! -d $C9_HOME ]; then
+	C9_HOME=/opt/c9
+fi
+
+if [ -d $C9_HOME ]; then 
+	sudo ln -s -f /opt/c9/bin/tmux /usr/bin/tmux
+fi
 
 cat << EOF >> /tmp/cast
 #!/bin/bash
 
 panes=\$(tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index}')
 
-for p in \$panes; do 
+for p in \$panes; do
     #echo Sending command to pane: \$p;
     tmux send-keys -t \$p "\$1 \$2 \$3 \$4 \$5 \$6 \$7 \$8 \$8 \$9" Enter;
 done
@@ -45,5 +54,4 @@ EOF
 chmod +x /tmp/cast
 sudo mv /tmp/cast /usr/bin/cast
 sudo ln -s -f /usr/bin/cast /usr/bin/c
-
 
