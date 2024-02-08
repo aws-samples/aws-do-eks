65f9b707085b0d0e309426fce81c8198462479d1
iankouls-aws <iankouls@amazon.com>
2024-02-08 07:22:47 +0000

    Upgrade mpi-operator

--- Container-Root/eks/deployment/kubeflow/mpi-operator/deploy.sh
+++ Container-Root/eks/deployment/kubeflow/mpi-operator/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubeflow/mpi-operator/v0.3.0/deploy/v2beta1/mpi-operator.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubeflow/mpi-operator/v0.4.0/deploy/v2beta1/mpi-operator.yaml
 
 # Add lease permissions fot mpi-operator cluster role
 kubectl apply -f ./clusterrole-mpi-operator.yaml
