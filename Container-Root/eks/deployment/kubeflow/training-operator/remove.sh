577c2314633ed7119cc0f2433a689b88ef491585
iankouls-aws <iankouls@amazon.com>
2024-02-21 05:40:58 +0000

    fsdp corrections

--- Container-Root/eks/deployment/kubeflow/training-operator/remove.sh
+++ Container-Root/eks/deployment/kubeflow/training-operator/remove.sh
@@ -8,5 +8,5 @@ kubectl delete -f ./clusterrole-hpa-access.yaml
 
 # Remove Kubeflow training operator
 
-kubectl delete -k "github.com/kubeflow/training-operator/manifests/overlays/standalone?ref=v1.5.0"
+kubectl delete -k "github.com/kubeflow/training-operator/manifests/overlays/standalone?ref=v1.7.0"
 
