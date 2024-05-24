ec8524a771a45a29f68adb9bd1855a77f8d13b2c
Alex Iankoulski <iankouls@amazon.com>
2024-05-24 00:59:45 +0000

    enable external mode selection for run command

--- run.sh
+++ run.sh
@@ -7,11 +7,13 @@
 
 source .env
 
-if [ -z "$1" ]; then
-	MODE=-d
-else
-	MODE=-it
-fi 
+if [ -z "$MODE" ]; then
+	if [ -z "$1" ]; then
+		MODE=-d
+	else
+		MODE=-it
+	fi 
+fi
 
 docker container run ${RUN_OPTS} ${CONTAINER_NAME} ${MODE} ${NETWORK} ${PORT_MAP} ${VOL_MAP} ${REGISTRY}${IMAGE}${TAG} $@
 
