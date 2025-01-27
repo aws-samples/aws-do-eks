26075d5f2923be36026b72acfc2593ffc7347fdf
Alex Iankoulski <iankouls@amazon.com>
2025-01-27 15:51:36 -0800

    Update EBS csi driver to v1.39

--- Container-Root/eks/deployment/csi/ebs/deploy.sh
+++ Container-Root/eks/deployment/csi/ebs/deploy.sh
@@ -2,5 +2,5 @@
 
 # Reference: https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/docs/install.md
 
-kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.27"
+kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.39"
 
