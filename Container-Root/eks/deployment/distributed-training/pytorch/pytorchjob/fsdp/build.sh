66b3ea087f2460f6d5a10d245abf45fd516ac146
Alex Iankoulski <iankouls@amazon.com>
2025-02-21 18:10:53 -0800

    Add fsdp_2 branch - checkpointing disabled

--- Container-Root/eks/deployment/distributed-training/pytorch/pytorchjob/fsdp/build.sh
+++ Container-Root/eks/deployment/distributed-training/pytorch/pytorchjob/fsdp/build.sh
@@ -9,5 +9,5 @@ fi
 
 export DOCKER_BUILDKIT=1
 #docker build --no-cache --progress=plain --build-arg="MODEL_NAME=$MODEL_NAME" -t ${REGISTRY}${IMAGE}${TAG} -f Dockerfile.$DOCKERFILE_EXT .
-docker build --progress=plain --build-arg="MODEL_NAME=$MODEL_NAME" -t ${REGISTRY}${IMAGE}${TAG} -f Dockerfile.$DOCKERFILE_EXT .
+docker build --progress=plain --build-arg="MODEL_NAME=$MODEL_NAME" -t ${REGISTRY}${IMAGE}${TAG} -f Dockerfile.$DOCKERFILE_EXT . $@
 
