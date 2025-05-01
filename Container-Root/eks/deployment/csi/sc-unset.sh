1cb6b5e53995c8822929abd962b738434fff5ce9
Alex Iankoulski <iankouls@amazon.com>
2025-04-30 19:16:10 -0700

    Add storage class config scripts

---
+++ Container-Root/eks/deployment/csi/sc-unset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Unsets default storage class
+# Reference: https://kubernetes.io/docs/tasks/administer-cluster/change-default-storage-class/
+
+help() {
+	echo ""
+	echo "Usage: $0 <storage_class_name>"
+	echo ""
+	echo "storage_class_name   - name of the storage class to unset as default"
+	echo ""
+}
+
+SC=$1
+
+if [ "$SC" == "" ]; then
+	help
+else
+	CMD="kubectl patch storageclass ${SC} -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}'"
+	if [ ! "$verbose" == "false" ]; then echo -e "\n${CMD}\n"; fi
+	eval "$CMD"
+fi
+
