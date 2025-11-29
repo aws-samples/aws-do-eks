0f7d121cbfbfb02cc8fe8a8d5e1098f9361421cd
Alex Iankoulski <iankouls@amazon.com>
2025-11-29 11:09:10 -0800

    use iftop for container name

--- Container-Root/eks/ops/iftop.sh
+++ Container-Root/eks/ops/iftop.sh
@@ -18,7 +18,7 @@ else
         node_name=$1
         full_node_name=$(kubectl get nodes | grep $node_name | head -n 1 | cut -d ' ' -f 1)
         short_node_name=$(echo $full_node_name | cut -d '.' -f 1)
-        pod_name=efatop-${short_node_name:-4}	
+        pod_name=iftop-${short_node_name:-4}	
         CMD="kubectl run -it --rm --privileged=true $pod_name --image iankoulski/efatop:latest --overrides='{\"apiVersion\": \"v1\", \"spec\": { \"nodeSelector\": { \"kubernetes.io/hostname\": \"$full_node_name\" }, \"volumes\": [ {\"name\": \"sys-vol\", \"hostPath\": { \"path\": \"/sys\", \"type\": \"\" }}], \"containers\":  [ { \"name\": \"iftop\", \"image\": \"iankoulski/efatop:latest\", \"tty\": true, \"stdin\": true, \"command\": [\"bash\",\"-c\",\"bmon\"], \"volumeMounts\": [{ \"name\": \"sys-vol\", \"mountPath\": \"/sys\"}] } ] } }'"
         if [ ! "$VERBOSE" == "false" ]; then echo -e "\n${CMD}\n"; fi
         eval "$CMD"
