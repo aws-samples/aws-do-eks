8a44a02c19bd4c789b730f12406c21d6812a5491
Alex Iankoulski <iankouls@amazon.com>
2026-05-23 20:04:33 +0000

    Add public ecr and other registries login functionality to login.sh

--- login.sh
+++ login.sh
@@ -8,5 +8,14 @@
 source ./.env
 
 # Login to container registry
-echo "Logging in to $REGISTRY ..."
-aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY
+if [ "$1" == "" ]; then
+	echo "Logging in to $REGISTRY ..."
+	aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY
+elif [ "$1" == "public" ]; then
+	echo "Logging in to public.ecr.aws ..."
+	aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+else 
+	echo "Logging in to $1 ..."
+	docker login $1
+fi
+
