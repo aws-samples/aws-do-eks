838bf9a63f5ef47aca90199874cc8a3001ccce07
Alex Iankoulski <iankouls@amazon.com>
2026-05-29 04:43:39 +0000

    update mpi-operator deployment

--- Container-Root/eks/deployment/kubeflow/mpi-operator/deploy.sh
+++ Container-Root/eks/deployment/kubeflow/mpi-operator/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubeflow/mpi-operator/v0.4.0/deploy/v2beta1/mpi-operator.yaml
+# Ref: https://github.com/kubeflow/mpi-operator
 
-# Add lease permissions fot mpi-operator cluster role
-kubectl apply -f ./clusterrole-mpi-operator.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/kubeflow/mpi-operator/v0.8.0/deploy/v2beta1/mpi-operator.yaml
+
+# Add lease permissions for mpi-operator cluster role
+#kubectl apply -f ./clusterrole-mpi-operator.yaml
--- Container-Root/eks/deployment/kubeflow/mpi-operator/remove.sh
+++ Container-Root/eks/deployment/kubeflow/mpi-operator/remove.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kubectl delete -f https://raw.githubusercontent.com/kubeflow/mpi-operator/v0.3.0/deploy/v2beta1/mpi-operator.yaml
+# Ref: https://github.com/kubeflow/mpi-operator
+
+kubectl delete -f https://raw.githubusercontent.com/kubeflow/mpi-operator/v0.8.0/deploy/v2beta1/mpi-operator.yaml
 
