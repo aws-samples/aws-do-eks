ad49a640652f573d7e19d1dd3d958655508fc850
Alex Iankoulski <iankouls@amazon.com>
2026-05-29 07:09:11 +0000

    Update nccl test image Dockerfiles

--- Container-Root/eks/deployment/nccl-test/cuda-efa-nccl-tests/build.sh
+++ Container-Root/eks/deployment/nccl-test/cuda-efa-nccl-tests/build.sh
@@ -3,7 +3,8 @@
 source .env
 
 # Build Docker image
-CMD="docker image build ${BUILD_OPTS} -t ${REGISTRY}${IMAGE}${TAG} ."
+#CMD="docker image build ${BUILD_OPTS} -t ${REGISTRY}${IMAGE}${TAG} ."
+CMD="docker image build ${BUILD_OPTS} -t ${REGISTRY}${IMAGE}${TAG} -f Dockerfile.public ."
 
 echo "$CMD"
 
