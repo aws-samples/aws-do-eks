c8f574523e9cb9c24fe044025bb1291e2a11d214
Alex Iankoulski <iankouls@amazon.com>
2026-05-29 22:17:44 -0700

    add pattern to fallocate utility

--- Container-Root/eks/deployment/csi/scratch/fallocate.sh
+++ Container-Root/eks/deployment/csi/scratch/fallocate.sh
@@ -6,5 +6,16 @@ if [ ! "$1" == "" ]; then
 	FILESIZE=$1
 fi
 
-kubectl exec -it emptydir-pod -- bash -c "fallocate -l $FILESIZE /scratch/largefile.dmp; ls -alh /scratch"
+pattern="^[0-9]{1,10}[a-zA-Z]{1,3}$"
+
+echo ""
+if [[ "$FILESIZE" =~ $pattern ]]; then
+    echo "Creating file /cratch/largefile.dmp of size $FILESIZE"
+    kubectl exec -it emptydir-pod -- bash -c "fallocate -l $FILESIZE /scratch/largefile.dmp; ls -alh /scratch"
+else
+    echo "Aborting file creation ..."
+    echo "Usage: $0 [FILESIZE]"
+    echo "Please specify FILESIZE as sequence of 1 to 10 digits followed by 1 to 3 characters, example: 100G"
+fi
+echo ""
 
