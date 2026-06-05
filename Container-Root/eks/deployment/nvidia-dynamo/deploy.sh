b4943cce94f638ed27b271840ce4dcb37203c8f9
Alex Iankoulski <iankouls@amazon.com>
2026-06-05 22:47:46 +0000

    Bump version of dynamo to 1.2.0, and aws-do-eks container to 20260605

--- Container-Root/eks/deployment/nvidia-dynamo/deploy.sh
+++ Container-Root/eks/deployment/nvidia-dynamo/deploy.sh
@@ -4,7 +4,7 @@
 # Ref: https://docs.nvidia.com/dynamo/kubernetes-deployment/deployment-guide/quickstart
 
 export DYNAMO_NAMESPACE=dynamo-system
-export DYNAMO_VERSION="1.1.1"
+export DYNAMO_VERSION="1.2.0"
 
 helm fetch https://helm.ngc.nvidia.com/nvidia/ai-dynamo/charts/dynamo-platform-${DYNAMO_VERSION}.tgz
 
