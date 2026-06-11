f3ca2c9dc6bc43f87ca9dd24a46832249e599629
iankouls-aws <iankouls@amazon.com>
2026-06-11 01:16:26 -0700

    Update install-go script to support arm64 architecture

--- Container-Root/eks/ops/setup/install-go.sh
+++ Container-Root/eks/ops/setup/install-go.sh
@@ -3,7 +3,9 @@
 # Install golang
 # https://go.dev/doc/install
 
-curl -o go.tar.gz --location "https://go.dev/dl/go1.20.linux-amd64.tar.gz"
+ARCH=$(uname -m)
+if [ "${ARCH}" == "aarch64" ]; then ARCH=arm64; fi
+curl -o go.tar.gz --location "https://go.dev/dl/go1.20.linux-${ARCH}.tar.gz"
 rm -rf /usr/local/go && tar -C /usr/local -xzf go.tar.gz
 rm -f ./go.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
